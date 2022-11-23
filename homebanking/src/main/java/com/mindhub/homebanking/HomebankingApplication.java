480d27e0b84c27defaf61c26f63030dc262d8c8e
Misael J. López <misaeljlo91@gmail.com>
2022-11-23 00:31:16 -0300

    Creación de clase y repositorio

--- homebanking/src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ homebanking/src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,7 +1,11 @@
 package com.mindhub.homebanking;
 
+import com.mindhub.homebanking.models.*;
+import com.mindhub.homebanking.repositories.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -10,4 +14,11 @@ public class HomebankingApplication {
 		SpringApplication.run(HomebankingApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository) {
+		return(args) -> {
+			Client client = new Client("Misael J.", "López", "misaeljlo91@gmail.com");
+			clientRepository.save(client);
+		};
+	}
 }
